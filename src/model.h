7367ca6b99ffd4eec9a1f0d09f20f3fe0a8fcc92
spykyvenator <spykyvenator@protonmail.com>
2024-07-01 21:03:55 +0200

    update: parse

--- src/model.h
+++ src/model.h
@@ -8,6 +8,7 @@
   // model.lp_.a_matrix_.start_ = {0, 2, 5};
   // model.lp_.a_matrix_.index_ = {1, 2, 0, 1, 2};
   // model.lp_.a_matrix_.value_ = {1.0, 3.0, 1.0, 2.0, 2.0};
+/*
 struct ll;
 
 typedef struct {
@@ -15,16 +16,20 @@ typedef struct {
     char *val;
     ll *next;
 } ll;
+*/
 
+/* 
 typedef struct {
     int num_col, num_row, num_nz, sense;
 
-    double *col_cost, *col_lower, *col_upper, *row_lower, *row_upper, offset, *a_value;
+    double col_cost[10], col_lower[10], col_upper[10], *row_lower, *row_upper, offset, a_value[10];
 
     int *a_start, *a_index;
 
-    ll *varNames;
+    int nIndex[2];
+    char *varNames[10];
 } model_t;
+*/
 
 typedef struct {
         double *col_value, *col_dual, *row_value, *row_dual, objective_value;
@@ -33,4 +38,5 @@ typedef struct {
 } sol;
 sol* solveModel(model_t *m);
 void printSol(sol *solution, model_t *mod);
+
 #endif
