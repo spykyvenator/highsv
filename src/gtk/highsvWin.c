5edb09adbe4f84346aed14318ee1a424f23597d3
spykyvenator <spykyvenator@protonmail.com>
2024-07-20 12:34:09 +0200

    try to open result file

--- src/gtk/highsvWin.c
+++ src/gtk/highsvWin.c
@@ -51,6 +51,16 @@ solveModel(GtkEntry *entry, HighsvAppWindow *win)
     content = gtk_text_buffer_get_text(buffer, &startI, &endI, TRUE);
 
     parseString(content);
+
+    GFileIOStream* stream = NULL;
+    GFile *new = g_file_new_tmp("result", &stream, NULL);
+    GOutputStream* ostream = g_io_stream_get_output_stream((GIOStream*) stream);
+    gsize bw;
+    g_output_stream_write_all(ostream, "testing\0", 8, &bw, NULL, NULL);
+    g_output_stream_close(ostream, NULL, NULL);
+    g_io_stream_close((GIOStream*)stream, NULL, NULL);
+
+    highsv_app_window_open(win, new);
     //printf("content: %s\n", content);
 }
 
