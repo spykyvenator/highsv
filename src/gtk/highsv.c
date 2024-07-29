5bcb73ee7819ca9044dce18510f4127e63ed2f68
spykyvenator <spykyvenator@protonmail.com>
2024-07-29 20:02:19 +0200

    add: quit shortcut

--- src/gtk/highsv.c
+++ src/gtk/highsv.c
@@ -16,6 +16,31 @@ highsv_app_init (HighsvApp *app)
 }
 
 static void
+quit_activated (GSimpleAction *action, GVariant *parameter, gpointer app)
+{
+  g_application_quit(G_APPLICATION(app));
+}
+
+
+static GActionEntry app_entries[] =
+{
+  { "quit", quit_activated, NULL, NULL, NULL }
+};
+
+static void
+highsv_app_startup(GApplication *app)
+{
+  const char *quit_accels[2] = { "<ctrl>q", NULL };
+
+  G_APPLICATION_CLASS(highsv_app_parent_class)->startup (app);
+
+  g_action_map_add_action_entries (G_ACTION_MAP (app),
+                                   app_entries, G_N_ELEMENTS (app_entries),
+                                   app);
+  gtk_application_set_accels_for_action(GTK_APPLICATION(app), "app.quit", quit_accels); 
+}
+
+static void
 highsv_app_activate (GApplication *app)
 {
   HighsvAppWindow *win;
@@ -52,8 +77,9 @@ highsv_app_open (GApplication *app, GFile **files, int n_files, const char *hint
 static void
 highsv_app_class_init (HighsvAppClass *class)
 {
-  G_APPLICATION_CLASS (class)->activate = highsv_app_activate;
-  G_APPLICATION_CLASS (class)->open = highsv_app_open;
+  G_APPLICATION_CLASS(class)->activate = highsv_app_activate;
+  G_APPLICATION_CLASS(class)->open = highsv_app_open;
+  G_APPLICATION_CLASS(class)->startup = highsv_app_startup;
 }
 
 HighsvApp *
