b31b8cec1c81eae666c10cd12c59ac79a7737ae9
spykyvenator <spykyvenator@protonmail.com>
2024-09-24 21:59:24 +0200

    add: slack or surplus

--- src/printOstream.h
+++ src/printOstream.h
@@ -37,11 +37,12 @@ printValues(const void *mod, GOutputStream *ostream, gsize bw, GError *error)
   char text[kHighsMaximumStringLength];
   const char RowText[] = "\n\nRow\t\t Slack or Surplus\tDual Price\n";
   double col_value[numCol], row_value[numRow], col_dual[numCol], row_dual[numRow],
-        col_reduced[numRow], reducedCost;
+        col_reduced[numRow], reducedCost, offset;
   HighsInt col_index[numRow];
   const HighsInt num_nz = Highs_getNumNz(mod);
 
   Highs_getSolution(mod, col_value, col_dual, row_value, row_dual);
+  Highs_getObjectiveOffset(mod, &offset);
 
   if (!g_output_stream_printf(ostream, &bw, NULL, &error, 
           "Objective value:\t\t%lf\nTotal Variables:\t\t\t%d\nTotal Constraints:\t\t%d\nTotal nonzeros:\t\t\t%d\n\nVariable\tValue\t\tReduced Cost\n",
@@ -93,6 +94,13 @@ printValues(const void *mod, GOutputStream *ostream, gsize bw, GError *error)
       g_clear_error(&error);
   }
 
+  if (!g_output_stream_printf(ostream, &bw, NULL, &error, 
+          "%s\t\t%lf\t\t\t%lf\n",
+          "0", objectiveVal - offset, 0.0)) {
+      g_printerr("Error writing to file: %s\n", error->message);
+      g_clear_error(&error);
+  }
+
   /*
    * slack/surplus = abs(diff of row)
    */
@@ -109,8 +117,10 @@ printValues(const void *mod, GOutputStream *ostream, gsize bw, GError *error)
       break;
     const double rowBound = MIN(ABS(lower), ABS(upper));
     double rowVal = 0;
-    for (int j = 0; j < rowNumNz; j++)
-      rowVal += col_value[j];
+    for (int j = 0; j < rowNumNz; j++) {
+      rowVal += matrix_value[j] * col_value[matrix_index[j]];
+    }
+    printf("row slack: %lf- %lf\n", rowVal, rowBound);
 
 
     if (!g_output_stream_printf(ostream, &bw, NULL, &error, 
