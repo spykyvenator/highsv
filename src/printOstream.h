3e5da19878941e667c8d997162c8b9625604df0a
spykyvenator <spykyvenator@protonmail.com>
2024-09-27 13:59:07 +0200

    add: start ranging print

--- src/printOstream.h
+++ src/printOstream.h
@@ -1,3 +1,6 @@
+/*
+ * range printing with Highs_getRanging
+ */
 #ifndef PRINTO_H
 #define PRINTO_H
 #include <highs/interfaces/highs_c_api.h>
@@ -5,46 +8,82 @@
 
 extern int numRow, numCol;
 
-//#define MIN(a,b) (a < b ? a : b)
-//#define ABS(a) (a < 0 ? -a : a)
-
-void
-pToF(GOutputStream *ostream, const char *str, ...)
+static void
+pToF(GOutputStream *ostr, const char *str, ...)
 {
   gsize bw;
   GError *error = NULL;
   va_list ap;
   va_start(ap, str);
-  if (!g_output_stream_vprintf(ostream, &bw, NULL, &error,
+  if (!g_output_stream_vprintf(ostr, &bw, NULL, &error,
         str, ap)) {
       g_printerr("Error writing to file: %s\n", error->message);
       g_clear_error(&error);
   }
   va_end(ap);
+}
+
+static void
+printEmpty(const void *mod, GOutputStream* ostr){
+  pToF(ostr,"Your model appears empty\n");
+}
+
+static void
+printInfeasible(const void *mod, GOutputStream* ostr){
+  pToF(ostr, "Your model is infeasible\n");
+}
+
+static double
+getSlack(const void *mod, const HighsInt row, const HighsInt num_nz, const double *r_value)
+{
+  int rowNumNz, matrix_index[num_nz];
+  HighsInt numRow, start;
+  double lower, upper, matrix_value[num_nz];
+  Highs_getRowsByRange(mod, row, row, &numRow, &lower, &upper, 
+        &rowNumNz, &start, 
+        matrix_index, matrix_value);
+  const double rowBound = MIN(ABS(lower), ABS(upper));
+#ifdef DEBUG
+  printf("row slack: %lf- %lf\n", rowVal, rowBound);
+#endif
+  return ABS(r_value[row]-rowBound);
 
 }
 
-void
-printEmpty(const void *mod, GOutputStream* ostream){
-  pToF(ostream,"Your model appears empty\n");
-}
+pRange(const void *mod, GOutputStream *ostr)
+{
+  double ccUpperVal, ccUpperObj, 
+         ccLowerVal, ccLowerObj,
+         cBndUpperValue, cBndUpperObj,
+         cBndLowerVal, cBndLowerObj,
+         rBndUpperVal, rBndUpperObj,
+         rBndLowerVal, rBndLowerObj;
 
-void
-printInfeasible(const void *mod, GOutputStream* ostream){
-  pToF(ostream, "Your model is infeasible\n");
+  HighsInt ccUpperInVar, ccUpperOutVar,
+         ccLowerInVar, ccLowerOutVar,
+         cBndUpInVar, cBndUpOutVar,
+         cBndLowerInVar, cBndLowerOutVar,
+         rBndUpperInVar, rBndUpperOutVar,
+         rBndLowerInVar, rBndLowerOutVar;
+
+  Highs_getRanging(mod, &ccUpperVal, &ccUpperObj, &ccUpperInVar, &ccUpperOutVar,
+      &ccLowerVal, &ccLowerObj, &ccLowerInVar, &ccLowerOutVar,
+      &cBndUpperValue, &cBndUpperObj, &cBndUpInVar, &cBndUpOutVar,
+      &cBndLowerVal, &cBndLowerObj, &cBndLowerInVar, &cBndLowerOutVar,
+      &rBndUpperVal, &rBndUpperObj, &rBndUpperInVar, &rBndUpperOutVar,
+      &rBndLowerVal, &rBndLowerObj, &rBndLowerInVar, &rBndLowerOutVar);
 }
 
 /*
  * TODO: clean this function up and split it into multiple funcs
  */
-void
-printValues(const void *mod, GOutputStream *ostream)
+static void
+pVal(const void *mod, GOutputStream *ostr)
 {
   gsize bw;
   GError *error = NULL;
   const double objectiveVal = Highs_getObjectiveValue(mod);
   char text[kHighsMaximumStringLength];
-  const char RowText[] = "\n\nRow\t\t Slack or Surplus\tDual Price\n";
   double col_value[numCol], row_value[numRow], col_dual[numCol], row_dual[numRow],
         col_reduced[numRow], reducedCost, offset;
   HighsInt col_index[numRow];
@@ -53,32 +92,28 @@ printValues(const void *mod, GOutputStream *ostream)
   Highs_getSolution(mod, col_value, col_dual, row_value, row_dual);
   Highs_getObjectiveOffset(mod, &offset);
 
-  pToF(ostream, "Objective value:\t\t%lf\nTotal Variables:\t\t\t%d\nTotal Constraints:\t\t%d\nTotal nonzeros:\t\t\t%d\n\nVariable\tValue\t\tReduced Cost\n", 
+  pToF(ostr, "Objective value:\t\t%lf\nTotal Variables:\t\t\t%d\nTotal Constraints:\t\t%d\nTotal nonzeros:\t\t\t%d\n\nVariable\tValue\t\tReduced Cost\n", 
+       
       objectiveVal, numCol, numRow, num_nz);
 
   for (uint8_t i = 0; i < numCol; i++){
     if (Highs_getColName(mod, i, text) == kHighsStatusError)// stop printing if variable has no name
       break;
-    HighsInt  reducedIndex;
+    HighsInt reducedIndex;
 
-    printf("num_nz: %d\n", num_nz);
+    printf("num_nz: %d col_dual %lf\n", num_nz, col_dual[i]);
     if (col_value[i] == 0.0 && signbit(col_value[i]))// remove negative 0's
         col_value[i] += 0.0;
 
     if (!col_value[i]){
       reducedCost = 0.0;
     } else {
-      HighsInt colNz;
-      if (Highs_getReducedColumn(mod, i, col_reduced, &colNz, col_index) == kHighsStatusError)
-        break;
-      reducedCost = col_reduced[0];
     }
-    
-    pToF(ostream, "%s\t\t%lf\t\t\t%lf\n",text, col_value[i], reducedCost);
+    pToF(ostr, "%s\t\t%lf\t\t\t%lf\n",text, col_value[i], 0.0);
   }
 
-  pToF(ostream, RowText);
-  pToF(ostream, "%s\t\t%lf\t\t\t%lf\n", "0", objectiveVal - offset, 0.0);
+  pToF(ostr, "\n\nRow\t\t Slack or Surplus\tDual Price\n%s\t\t%lf\t\t\t%lf\n", 
+      "0", objectiveVal - offset, 0.0);
 
   /*
    * slack/surplus = abs(diff of row)
@@ -86,46 +121,33 @@ printValues(const void *mod, GOutputStream *ostream)
   for (uint8_t i = 0; i < numRow; i++){
     if (Highs_getRowName(mod, i, text) == kHighsStatusError)// stop printing if variable has no name
       break;
-
-    HighsInt numRow, start;
-    int rowNumNz, matrix_index[num_nz];
-    double lower, upper, matrix_value[num_nz];
-    if (Highs_getRowsByRange(mod, i, i, &numRow, &lower, &upper, 
-          &rowNumNz, &start, 
-          matrix_index, matrix_value) == kHighsStatusError)
-      break;
-    const double rowBound = MIN(ABS(lower), ABS(upper));
-    double rowVal = 0;
-    for (int j = 0; j < rowNumNz; j++) {
-      rowVal += matrix_value[j] * col_value[matrix_index[j]];
-    }
-    printf("row slack: %lf- %lf\n", rowVal, rowBound);
-
-
-    pToF(ostream, "%s\t\t%lf\t\t\t%lf\n", text,  ABS(rowVal-rowBound), 0.0);
+    printf("row_dual %lf,row_value %lf\n", row_dual[i], row_value[i]);
+    pToF(ostr, "%s\t\t%lf\t\t\t%lf\n", text,  getSlack(mod, i, num_nz, row_value), 0.0);
   }
 }
 
-void
-printUnbounded(const void *mod, GOutputStream *ostream){
-  pToF(ostream, "Your model is unbounded\n");
-  printValues(mod, ostream);
+static void
+pUnbound(const void *mod, GOutputStream *ostr){
+  pToF(ostr, "Your model is unbounded\n");
+  pVal(mod, ostr);
 }
 
-void
-printError(const void *mod, GOutputStream *ostream){
-  pToF(ostream, "Something went wrong\n");
+static void
+pErr(const void *mod, GOutputStream *ostr){
+  pToF(ostr, "Something went wrong\n");
 }
 
-void
-printOptimal(const void *mod, GOutputStream *ostream){
-  pToF(ostream, "Global optimal solution found:\n");
-  printValues(mod, ostream);
+static void
+pOpt(const void *mod, GOutputStream *ostr){
+  pToF(ostr, "Global optimal solution found:\n");
+  pVal(mod, ostr);
 }
 
-void
-printSolToFile(const void *mod, GOutputStream* ostream) {
+static void
+printSolToFile(const void *mod, GOutputStream* ostr) {
   HighsInt status = Highs_getModelStatus(mod);
+  pToF(ostr, "HiGHS Version: %d.%d.%d\n",
+    Highs_versionMajor(), Highs_versionMinor(), Highs_versionPatch());
 
   if (status == kHighsModelStatusNotset
     || status == kHighsModelStatusLoadError
@@ -138,15 +160,15 @@ printSolToFile(const void *mod, GOutputStream* ostream) {
     || status == kHighsModelStatusUnknown
     || status == kHighsModelStatusSolutionLimit
     || status == kHighsModelStatusInterrupt)
-    printError(mod, ostream);
+    pErr(mod, ostr);
   else if (status == kHighsModelStatusModelEmpty)
-    printEmpty(mod, ostream);
+    printEmpty(mod, ostr);
   else if (status == kHighsModelStatusOptimal)
-    printOptimal(mod, ostream);
+    pOpt(mod, ostr);
   else if (status == kHighsModelStatusInfeasible 
       || status == kHighsModelStatusUnboundedOrInfeasible)
-    printInfeasible(mod, ostream);
+    printInfeasible(mod, ostr);
   else if (status == kHighsModelStatusUnbounded)
-    printUnbounded(mod, ostream);
+    pUnbound(mod, ostr);
 }
 #endif
