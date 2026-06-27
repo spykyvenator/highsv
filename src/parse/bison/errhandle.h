bb388348f1868d0250ca7d99c00cc68c86655a97
spykyvenator <spykyvenator@protonmail.com>
2026-06-27 13:44:22 +0200

    add: errhandle

---
+++ src/parse/bison/errhandle.h
@@ -0,0 +1,47 @@
+#ifndef ERRHANDLE_H
+#define ERRHANDLE_H
+#ifdef CLI
+typedef struct errHandle {
+    void (*pErr)(errHandle *self, const char *msg);
+} errHandle;
+#elif GTK
+#include <gtk/gtk.h>
+#include "../../gtk/highsvWin.h"
+#include "../../util.h"
+
+typedef struct errHandle errHandle;
+struct errHandle {
+    void (*pErr)(errHandle *self, const char *msg, int x, int y, int x2, int y2);
+    void (*free)(errHandle *self);
+    GtkWidget *view;
+    GtkTextBuffer *bfr;
+};
+
+static void
+gtk_errHandler(errHandle *self, const char *msg, int x, int y, int x2, int y2)
+{
+    highsvShowError(msg, self->view, self->bfr, x, y, x2, y2);
+}
+
+static void
+freeEh(errHandle *self)
+{
+    free(self);
+}
+
+static inline errHandle*
+new_errHandle(GtkWidget *view, GtkTextBuffer *bfr)
+{
+    errHandle *res = (errHandle*) h_malloc(sizeof(errHandle));
+    res->view = view;
+    res->bfr = bfr;
+    res->pErr = gtk_errHandler;
+    res->free = freeEh;
+    return res;
+}
+#else
+typedef struct errHandle {
+    void (*pErr)(errHandle *self, const char *msg);
+} errHandle;
+#endif
+#endif
